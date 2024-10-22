82dede4e4121a7328cb6988a03ac5f2a78c20e8d
Sebastian Uddén <sebastian.udden@gmail.com>
2024-10-22 22:16:13 +0200

    add password protection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const links = [
@@ -19,25 +20,35 @@ const links = [
   },
 ];
 
-export const Home = () => (
-  <div className="p-10">
-    <h1>BOSS WinterGames</h1>
-    <div className="flex flex-wrap gap-6 mt-10">
-      {links.map(({ label, to, image }) => (
-        <Link
-          key={label}
-          to={to}
-          className={cn(
-            "relative text-4xl text-white bg-black rounded-xl hover:text-white aspect-square flex items-center justify-center group w-full max-w-[50vh]",
-            `bg-${image} bg-cover`
-          )}
-        >
-          <div className="w-full text-center">{label}</div>
-          <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 bg-black opacity-70 group-hover:opacity-0 group-active:opacity-0 max-w-[50vh]">
-            {label}
-          </div>
-        </Link>
-      ))}
+export const Home = () => {
+  const [password, setPassword] = useState("");
+  return (
+    <div className="p-10">
+      <h1>BOSS WinterGames</h1>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        className="p-4 text-white"
+      />
+      <div className="flex flex-wrap gap-6 mt-10">
+        {password === "argh" &&
+          links.map(({ label, to, image }) => (
+            <Link
+              key={label}
+              to={to}
+              className={cn(
+                "relative text-4xl text-white bg-black rounded-xl hover:text-white aspect-square flex items-center justify-center group w-full max-w-[50vh]",
+                `bg-${image} bg-cover`
+              )}
+            >
+              <div className="w-full text-center">{label}</div>
+              <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 bg-black opacity-70 group-hover:opacity-0 group-active:opacity-0 max-w-[50vh]">
+                {label}
+              </div>
+            </Link>
+          ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
